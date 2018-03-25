0f5d001a4e668ce184e808f7caa002b406bb321b
hisiphp <chenf4hua12@qq.com>
2018-03-25 21:30:54 +0800

    ......
    升级thinkphp到5.0.15
    修复云推送参数错误问题
    修复会员等级批量设置无效
    修复关闭开发模式后台登录权限出错的问题
    修复访问index.html进到默认模块的BUG
    修复系统菜单无所属插件选项
    框架模式增加批量删除功能
    增加钩子关联的插件状态设置
    后台强制关闭路由
    优化在线升级功能
    提示无访问权限后跳转死循环
    创建管理员增加密码确认

--- version.php
+++ version.php
@@ -12,7 +12,7 @@ return [
     'hisiphp' => [
         'name' => 'HisiPHP',
         'version' => '1.0.3',
-        'release' => 20180206,
+        'release' => 20180325,
         'copyright' => 'HisiPHP.COM',
         'url' => 'http://www.hisiphp.com?v=thinkphp5',
     ],
